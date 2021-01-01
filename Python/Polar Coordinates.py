a55f87fff22d7009e6ee2750982fd06cc4fa753e
CJL <cesarjaitmanlabaton@gmail.com>
2021-01-01 15:24:12 -0800

    Tutorial DefaultDict

--- Python/Polar Coordinates.py
+++ Python/Polar Coordinates.py
@@ -6,7 +6,6 @@ z = '1+2j'
 # print(*cmath.polar(complex(z), sep='\n'))
 
 # Using regex:
-
 x, y = map(float, re.findall('[+-]?\d+\.?\d*', z.strip()))
 r = math.sqrt((x**2) + (y**2))
 yy = cmath.phase(complex(x, y))
